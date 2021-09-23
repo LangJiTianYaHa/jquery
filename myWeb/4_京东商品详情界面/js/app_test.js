c14a5d5364e8fd1b300f61147f7eb84051fd8167
LangJiTianYaHa <2414965060@qq.com>
2021-09-23 16:21:39 +0800

    first commit

---
+++ myWeb/4_京东商品详情界面/js/app_test.js
@@ -0,0 +1,401 @@
+/*
+ 1. 鼠标移入显示,移出隐藏
+ 目标: 手机京东, 客户服务, 网站导航, 我的京东, 去购物车结算, 全部商品
+ 2. 鼠标移动切换二级导航菜单的切换显示和隐藏
+ 3. 输入搜索关键字, 列表显示匹配的结果
+ 4. 点击显示或者隐藏更多的分享图标
+ 5. 鼠标移入移出切换地址的显示隐藏
+ 6. 点击切换地址tab
+
+ 7. 鼠标移入移出切换显示迷你购物车
+ 8. 点击切换产品选项 (商品详情等显示出来)
+
+ 9. 点击向右/左, 移动当前展示商品的小图片
+ 10. 当鼠标悬停在某个小图上,在上方显示对应的中图
+ 11. 当鼠标在中图上移动时, 显示对应大图的附近部分区域
+ */
+
+/*
+ 1. 对哪个/些元素绑定什么监听?
+ 2. 对哪个/些元素进行什么DOM操作?
+ */
+$(function () {
+  //函数方法调用
+  showhide()
+  hoverSubMenu()
+  search()
+  share()
+  clickTabs()
+  address()
+  hoverMiniCart()
+  clickProductTabs()
+  moveMiniImg()
+  hoverMiniImg()
+  bigImg()
+
+
+
+
+  /*11. 当鼠标在中图上移动时, 显示对应大图的附近部分区域*/
+  function  bigImg () {
+    //获取相应的元素
+    var $mediumImg = $('#mediumImg')
+    var $mask = $('#mask') // 小黄块
+    var $maskTop = $('#maskTop')
+    var $largeImgContainer = $('#largeImgContainer')
+    var $loading = $('#loading')
+    var $largeImg = $('#largeImg')
+    var maskWidth = $mask.width()
+    var maskHeight = $mask.height()
+    var maskTopWidth = $maskTop.width()
+    var maskTopHeight = $maskTop.height()
+    //hover 进入
+    $maskTop.hover(function () {
+      $mask.show()
+
+      //动态加载大图
+      // images\products\product-s2-m.jpg
+      // images/products/product-s2-l.jpg
+      var src  = $mediumImg.attr('src').replace('-m.','-l.')
+      $largeImg.attr('src',src)
+      $largeImgContainer.show()
+      //绑定加载监听
+      $largeImg.on('load',function () {
+        //得到大图的尺寸
+        var largeWidth = $largeImg.width()
+        var largeHeight = $largeImg.height()
+
+        //显示大图
+        $largeImg.show()
+        //隐藏进度条
+        $loading.hide()
+
+        //鼠标移动的监听
+        $maskTop.mousemove(function (event) {
+          /*
+           * 1.移动小黄块
+           * 2.移动大图
+           * */
+          //1.移动小黄块
+          //计算left值 top值
+          //计算left/top
+          var left = 0
+          var top = 0
+          // 事件的坐标
+          var eventLeft = event.offsetX
+          var eventTop = event.offsetY
+          left = eventLeft - maskWidth/2
+          top = eventTop - maskHeight/2
+          // left在[0, maskTopWidth-maskWidth]
+          if(left<0) {
+            left = 0
+          } else if(left>maskTopWidth-maskWidth) {
+            left = maskTopWidth-maskWidth
+          }
+          // top在[0, maskTopHeight-maskHeight]
+          if(top<0) {
+            top = 0
+          } else if(top>maskTopHeight-maskHeight) {
+            top = maskTopHeight-maskHeight
+          }
+          //给$mask重新定位
+          $mask.css({
+            left: left,
+            top: top
+          })
+
+          //2.移动大图
+          //得到大图的坐标
+          left = -left * largeWidth/maskTopWidth
+          top = -top * largeHeight/maskTopHeight
+          //设置大图的坐标
+          $largeImg.css({
+            left:left,
+            top:top
+          })
+
+
+
+        })
+
+      })
+
+
+
+
+    },function () {
+      //hover 移出
+      $mask.hide()
+      $largeImgContainer.hide()
+      $largeImg.hide()
+    })
+
+
+
+  }
+
+
+
+  /**10. 当鼠标悬停在某个小图上,在上方显示对应的中图*/
+
+  function hoverMiniImg() {
+    $('#icon_list > li ').hover(function () {
+      //移入  添加class 显示中图
+      var $img = $(this).children()
+      $img.addClass('hoveredThumb')
+
+      var src =  $img.attr('src').replace('.jpg','-m.jpg')
+      $('#mediumImg').attr('src',src)
+
+    },function () {
+      //移出
+      $(this).children().removeClass('hoveredThumb')
+    })
+  }
+
+  
+
+
+  /*
+   9. 点击向右/左, 移动当前展示商品的小图片
+   */
+  function moveMiniImg() {
+    //获取元素
+    var $as = $('#preview>h1>a')
+    var $backward = $as.first()
+    var  $forward = $as.last()
+
+    var $Ul = $('#icon_list')
+    var SHOW_COUNT = 5
+    var imgCount = $Ul.children('li').length
+    var moveCount = 0   //移动的次数(向右为正, 向左为负)
+    var liWidth = $Ul.children(':first').width()
+
+    //初始化更新  判断是否需要移动
+    if(imgCount > SHOW_COUNT){
+      $forward.attr('class','forward')
+    }
+    //为$forward设置点击监听
+    $forward.click(function () {
+      //如果不能移动直接退出
+      if(moveCount == imgCount - SHOW_COUNT){
+        return
+      }
+      //移动一次
+      moveCount++
+      //设置$backward
+      $backward.attr('class','backward')
+      //设置$forward
+      if(moveCount == imgCount - SHOW_COUNT){
+        $forward.attr('class','forward_disabled')
+      }
+      //设置ul
+      $Ul.css({
+        left: -liWidth*moveCount
+      })
+
+    })
+
+    //为$backward设置点击监听
+    $backward.click(function () {
+      //如果不能移动直接退出
+      if(moveCount == 0){
+        return
+      }
+      //移动一次
+      moveCount--
+      //设置$forward
+      $forward.attr('class','forward')
+      //设置$forward
+      if(moveCount == 0) {
+        $backward.attr('class','backward_disabled')
+      }
+      //设置ul
+      $Ul.css({
+        left: -liWidth*moveCount
+      })
+
+    })
+
+  }
+
+  /*
+  * 8.点击切换产品选项
+  *
+  * */
+  function clickProductTabs() {
+    //获取li 和 div
+    var $lis  =     $('#product_detail>ul>li')
+    var  $divs = $('#product_detail>div:gt(0)')
+    //设置点击监听
+    $lis.click(function () {
+      $lis.removeClass('current')
+      this.className = 'current'
+      $divs.hide()
+      var index = $(this).index()
+      // $divs.eq(index).show()
+      $divs[index].style.display = 'block'
+    })
+
+  }
+
+
+
+
+  /*  7. 鼠标移入移除显示迷你购物车
+  * */
+  function  hoverMiniCart () {
+    $('#minicart').hover(function () {
+      //移入
+      this.className = 'minicart'
+      $(this).children(':last').show('fast')
+    },function () {
+      //移出
+      this.className = ''
+      $(this).children(':last').hide('fast')
+
+    })
+
+  }
+
+  /*
+   6. 点击切换地址tab
+   */
+  function clickTabs() {
+    $('#store_tabs>li').click(function () {
+      $('#store_tabs>li').removeClass('hover')
+      //this.className = 'hover'
+      $(this).addClass('hover')
+    })
+  }
+
+  /*
+   5. 鼠标移入移出切换地址的显示隐藏
+   */
+  function address() {
+    var $select  = $('#store_select')
+    $select.hover(function () {
+      $(this).children(':gt(0)').show()
+    },function () {
+      $(this).children(':gt(0)').hide()
+    })
+      .children(':last').click(function () {
+      //alert(this.id)  this 是id为store_close的div
+      $(this).prevAll('div:lt(2)').hide()
+
+    })
+
+  }
+
+
+  /*
+   4. 点击显示或者隐藏更多的分享图标
+   */
+  /*function share () {
+    var isOpen = false //标识当前的状态(初始为关闭)
+    var $shareMore = $('#shareMore')
+    var $parent = $shareMore.parent()
+    var $as = $shareMore.prevAll('a:lt(2)')
+    var $b = $shareMore.children()
+
+    $shareMore.click(function () {
+
+      if(isOpen) { // 去关闭
+        isOpen = false
+        $parent.css('width', 155)
+        $as.hide()
+        $b.removeClass('backword')
+      } else { // 去打开
+        isOpen = true
+        $parent.css('width', 200)
+        $as.show()
+        $b.addClass('backword')
+      }
+
+      // isOpen = !isOpen
+    })
+  }*/
+  function  share () {
+    //获取当前状态 初始为关闭
+    var isOpen  = false
+    var $shareMore  = $('#shareMore')
+    var $parent = $shareMore.parent()
+    var $b = $shareMore.children()
+    var  $as  = $shareMore.prevAll('a:lt(2)')
+
+    $shareMore.click(function () {
+      if(isOpen){   //去关闭
+        isOpen = false
+        $parent.css('width', 155)
+        $as.hide()
+        $b.removeClass('backword')
+      }else{    //qu去开启
+        isOpen = true
+        $parent.css('width',200)
+        $as.show()
+        $b.addClass('backword')
+      }
+    })
+
+
+
+  }
+
+
+  /*
+   3. 输入搜索关键字, 列表显示匹配的结果
+   */
+
+  function search() {
+   $('#txtSearch').on('keyup focus', function () {
+     //事件回调函数中的this是DOM元素
+    // alert(this) //HTMLInputElement
+   var txt = this.value.trim()
+   //输入框有文字才显示列表
+   if (txt) {
+   $('#search_helper').show()
+   }
+   }).blur(function () {
+   //隐藏列表
+   $('#search_helper').hide()
+   })
+   }
+
+  /*
+   2. 鼠标移动切换二级导航菜单的切换显示和隐藏
+   */
+
+  function hoverSubMenu () {
+   $('#category_items>div').hover(function () {
+
+     //alert(this)  //HTMLDivElement
+
+   $(this).children(':last').show()
+   }, function () {
+   $(this).children(':last').hide()
+   })
+   }
+
+  function showhide() {
+    /*
+     1. 鼠标移入显示,移出隐藏
+     目标: 手机京东, 客户服务, 网站导航, 我的京东, 去购物车结算, 全部商品
+     */
+    $('[name = show_hide]').hover(function () {
+      var id = this.id + '_items'
+      $('#' + id).show()
+    }, function () {
+      var id = this.id + '_items'
+      $('#' + id).hide()
+
+    })
+  }
+
+
+
+
+
+
+
+})
+
---
+++ myWeb/JS/myLib.js
@@ -0,0 +1,8 @@
+/**
+ * Created by PC on 2021/4/5.
+ */
+(function () {
+  window.$ = function () {
+    console.log('my Lib ....')
+  }
+})()
